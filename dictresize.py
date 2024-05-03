a8dd29fc3af30cb27c9e5c54475682479b9d7a62
Karl Knechtel <zahlman@proton.me>
2024-05-03 13:55:17 -0400

    Refactoring
    Format strings are supplied externally so as to enable an alternate
    "raw" output format.

--- dictresize.py
+++ dictresize.py
@@ -13,31 +13,48 @@ def resizes(keys):
 
 # Determine when the dict resizes if 0 or more keys are deleted from a 5-key
 # dict and then a new key is added.
-def display_resizes():
+def display_resizes(format_string):
     for key_count in range(6):
         trials = list(itertools.combinations('abcde', key_count))
         fails = [keys for keys in trials if resizes(keys)]
-        print('{} keys removed: {}/{} resizes'.format(key_count, len(fails), len(trials)))
+        print(format_string.format(key_count, len(fails), len(trials)))
 
 
 # Determine base sizes of 0-, 5-, and 10-element dicts with varying types.
-def display_sizes():
-    print('empty dict size: {}'.format(memsize({})))
-    print('5-key letter-to-number dict size: {}'.format(
-        memsize({chr(a+ord('a')):a for a in range(5)})
-    ))
-    print('5-key number-to-letter dict size: {}'.format(
-        memsize({a:chr(a+ord('a')) for a in range(5)})
-    ))
-    print('10-key letter-to-number dict size: {}'.format(
-        memsize({chr(a+ord('a')):a for a in range(10)})
-    ))
-    print('10-key number-to-letter dict size: {}'.format(
-        memsize({a:chr(a+ord('a')) for a in range(10)})
-    ))
+def display_sizes(format_strings):
+    dicts = [
+        {},
+        {chr(a+ord('a')):a for a in range(5)},
+        {a:chr(a+ord('a')) for a in range(5)},
+        {chr(a+ord('a')):a for a in range(10)},
+        {a:chr(a+ord('a')) for a in range(10)}
+    ]
+    for f, d in zip(format_strings, dicts):
+        print(f.format(memsize(d)))
+
+
+def get_resize_format(name):
+    return {
+        'raw': '{1}',
+        'full': '{} keys removed: {}/{} resizes'
+    }[name]
+
+
+def get_size_format(name):
+    return {
+        'raw': ['{}'] * 5,
+        'full': [
+            'empty dict size: {}',
+            '5-key letter-to-number dict size: {}',
+            '5-key number-to-letter dict size: {}',
+            '10-key letter-to-number dict size: {}',
+            '10-key number-to-letter dict size: {}'
+        ]
+    }[name]
 
 
 if __name__ == '__main__':
-    display_sizes()
+    format_name = 'full'
+    display_sizes(get_size_format(format_name))
     print('') # support 2.7
-    display_resizes()
+    display_resizes(get_resize_format(format_name))
