e95f317db80c7a1eaf06d1f52f16b43fbac1cb19
Karl Knechtel <zahlman@proton.me>
2024-05-03 20:05:15 -0400

    make generate.sh do everything in one step

--- generate.sh
+++ generate.sh
@@ -3,5 +3,8 @@
 mkdir -p output
 for version in 2.7 3.5 3.6 3.7 3.8 3.9 3.10 3.11 3.12
 do
-    python$version dictresize.py $1 > output/$1$version.txt
+    python$version dictresize.py raw > output/raw$version.txt
+    python$version dictresize.py full > output/full$version.txt
 done
+# Assuming `python` maps to a 3.6 or better version.
+python make_csv.py
